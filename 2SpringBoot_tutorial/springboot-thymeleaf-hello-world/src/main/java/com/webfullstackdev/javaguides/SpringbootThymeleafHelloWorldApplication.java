66ba911683d9c0c5404549311f2cb6ae9423a547
gnganapath <gnganapathi@gmail.com>
2020-08-13 17:52:09 +0530

    2 tut - spb beginner to advance

---
+++ 2SpringBoot_tutorial/springboot-thymeleaf-hello-world/src/main/java/com/webfullstackdev/javaguides/SpringbootThymeleafHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.webfullstackdev.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafHelloWorldApplication.class, args);
+	}
+
+}
---
+++ 2SpringBoot_tutorial/springboot-thymeleaf-hello-world/src/test/java/com/webfullstackdev/javaguides/SpringbootThymeleafHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfullstackdev.javaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
