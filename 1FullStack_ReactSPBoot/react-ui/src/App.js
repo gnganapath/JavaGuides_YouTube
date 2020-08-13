bf53e5ad0de334b4dea27a0b91445a59931636a0
gnganapath <gnganapathi@gmail.com>
2020-08-13 17:40:18 +0530

    React plain project, spring boot with Dep - web, mariaDB, JPA,

---
+++ 1FullStack_ReactSPBoot/react-ui/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      React JS 16.13 for UI && spring boot 2.3.2 for (server scrpipt) middle ware && Maria DB 10.4 as DB  - Dev Tech stack
+    </div>
+  );
+}
+
+export default App;
