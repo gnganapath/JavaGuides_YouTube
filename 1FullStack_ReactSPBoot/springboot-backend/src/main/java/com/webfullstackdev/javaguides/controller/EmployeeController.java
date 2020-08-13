48b4312fb1d4dc5860a7a8db89f8223b04efa7fa
gnganapath <gnganapathi@gmail.com>
2020-08-13 19:56:19 +0530

    video-6 get All employee Get API with sample react app

---
+++ 1FullStack_ReactSPBoot/springboot-backend/src/main/java/com/webfullstackdev/javaguides/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.webfullstackdev.javaguides.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webfullstackdev.javaguides.model.Employee;
+import com.webfullstackdev.javaguides.service.EmployeeService;
+
+//Without service direct calling DAO purpose this import added for getAllEmployeesDAO()
+import com.webfullstackdev.javaguides.dao.EmployeeDao;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	// Calling crtl -> service -> Dao.findAll() 
+	@GetMapping("/employee")
+	public List<Employee> getAllEmployees(){
+	 return employeeService.getAllEmployees();
+	}
+	
+	
+	// Without service direct calling form controller to DAO for fetch JPA findAll()
+	// BEGIN ctrl - DAO
+	@Autowired
+	EmployeeDao employeeRepository;	
+	
+	@GetMapping("/contrl-employees")
+	public List < Employee > getAllEmployeesDAO() {
+	  return employeeRepository.findAll();
+	}
+	// END crtl - DAO
+
+}
---
+++ 1FullStack_ReactSPBoot/springboot-backend/src/main/java/com/webfullstackdev/javaguides/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.webfullstackdev.javaguides.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.webfullstackdev.javaguides.dao.EmployeeDao;
+import com.webfullstackdev.javaguides.model.Employee;
+
+@Service
+public class EmployeeService {
+	@Autowired
+	EmployeeDao employeeRepository;
+	
+	public List<Employee>  getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	public List<Employee> getAllEmployeess(){
+		return employeeRepository.findAll();
+	}
+	
+}
